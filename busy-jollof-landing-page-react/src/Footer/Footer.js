a144cfe0fc912073a9dfb47aea374242677d81ba
Victor  Nite <brainbox@victors-mini.myfiosgateway.com>
2023-07-25 23:31:49 -0400

    responsivess work on nav menu and footer menu

--- busy-jollof-landing-page-react/src/Footer/Footer.js
+++ busy-jollof-landing-page-react/src/Footer/Footer.js
@@ -3,48 +3,50 @@ import "./Footer.css"; // Import the CSS file for styling
 
 const Footer = () => {
   return (
-   
     <div className="div-wrapper">
       <div className="footer-container">
         {/* First Column */}
         <div className="column">
-          <div className="row logo">Logo</div>
-          <div className="row description">Description</div>
+          <div className="row"><img className="logo" src="./bj-footer-logo.png" alt="logo"></img></div>
+          <div className="row description">The future of online African food ordering <br/>and delivery</div>
           <div className="row social-icons">
-            <i className="social-icon">Social Icon 1</i>
-            <i className="social-icon">Social Icon 2</i>
-            <i className="social-icon">Social Icon 3</i>
+            <i className="social-icon"><img className="sm-icon" src="./insta.png" alt="logo"/></i>
+            <i className="social-icon"><img className="sm-icon" src="./Twiter.png" alt="logo"/></i>
+            <i className="social-icon"><img className="sm-icon" src="./Fb.png" alt="logo"/></i>
+            
           </div>
         </div>
 
         {/* Second Column */}
         <div className="column">
           <div className="row bold-text">Pages</div>
-          <div className="row">Page 1</div>
-          <div className="row">Page 2</div>
-          <div className="row">Page 3</div>
-          <div className="row">Page 4</div>
-          <div className="row">Page 5</div>
-          <div className="row">Page 6</div>
+          <div className="row">Home</div>
+          <div className="row">About us</div>
+          <div className="row">Blog</div>
+          <div className="row">Launch with us</div>
+          <div className="row">Our TimeLine</div>
+          <div className="row">Contact us</div>
         </div>
 
         {/* Third Column */}
         <div className="column">
-          <div className="row">Row 1</div>
-          <div className="row">Row 2</div>
-          <div className="row">Row 3</div>
-          <div className="row">Row 4</div>
+          <div className="row bold-text">Our Company</div>
+          <div className="row">Privacy policy</div>
+          <div className="row">About us</div>
+          <div className="row">FAQ</div>
         </div>
 
         {/* Fourth Column */}
         <div className="column">
-          <div className="row">Row 1</div>
-          <div className="row">Row 2</div>
+          <div className="row bold-text">For more information</div>
+          <div className="row">
+            Contact us at info@busyjollof.com or click the button below to send
+            us a message
+          </div>
           <button className="button">Button</button>
         </div>
       </div>
-      </div>
-    
+    </div>
   );
 };
 
--- busy-jollof-landing-page-react/src/HomePage/Home.js
+++ busy-jollof-landing-page-react/src/HomePage/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Home.css";
+//import Footer from "../Footer/Footer";
 
 export const Home = () => {
   return (
     <div className="div-container">
        <h1>Homepage</h1>
+       {/* <Footer /> */}
     </div>
    
   );
